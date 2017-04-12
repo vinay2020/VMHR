fa0c8a1b1146a78c20814b044bc8efbda2f85425
root <root@cea2b93bf2f3>
2017-04-12 21:09:23 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "material",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "VMHR",
+  "homePage" : "Main",
+  "name" : "VMHR",
+  "platformType" : "WEB",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
